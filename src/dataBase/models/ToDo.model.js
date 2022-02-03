b5345f731ff1c2b2a8b61ad5e23078de4f35011a
Guzel-Galieva <guzel.vv.com@gmail.ru>
2022-02-03 11:47:04 +0300

    бэк

---
+++ src/dataBase/models/ToDo.model.js
@@ -0,0 +1,37 @@
+const Sequelize = require("sequelize");
+const { sequelize } = require("..");
+
+class ToDo extends Sequelize.Model {}
+
+ToDo.init(
+  {
+    id: {
+      type: Sequelize.DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.DataTypes.UUIDV4,
+    },
+    title: {
+      type: Sequelize.STRING,
+      defaultValue: "Title",
+    },
+    description: {
+      type: Sequelize.STRING,
+      defaultValue: "Description",
+    },
+    isCompleted: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
+    isFavourite: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
+    priority: {
+      type: Sequelize.SMALLINT,
+      defaultValue: 1,
+    },
+  },
+  { sequelize: sequelize, underscored: true, modelName: "todo" }
+);
+
+module.exports = ToDo;
